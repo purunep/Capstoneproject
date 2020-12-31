4677ba76fe4f9c18e4803a8ffe38a01ae1de17e4
Puru <purunep@live.com>
2020-12-31 21:07:45 +0000

    Add files via upload

--- project/score.py
+++ project/score.py
@@ -1,20 +1,25 @@
 import json
 import numpy as np
 import os
+import pickle
 import joblib
+import pandas as pd
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
-    print("Found model:", os.path.isfile(model_path))
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
+    # For multiple models, it points to the folder containing all deployed models (./azureml-models)
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'hypermodel.pkl')
     model = joblib.load(model_path)
 
-def run(data):
+def run(raw_data):
     try:
-        data = np.array(json.loads(data))
-        result = model.predict(data)
-        # You can return any data type, as long as it is JSON serializable.
-        return result.tolist()
-    except Exception as e:
-        error = str(e)
+        data = json.loads(raw_data)['data']
+        data = pd.DataFrame.from_dict(data)
+        # make prediction
+        mypredict = model.predict(data)
+        return mypredict.tolist()
+    except Exception as ex:
+        error = str(ex)
         return error
