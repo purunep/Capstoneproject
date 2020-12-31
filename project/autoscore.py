d413d5d29f65a658be18bc414e63d19c0d279b43
Puru <purunep@live.com>
2020-12-31 19:04:21 +0000

    Add files via upload

--- project/autoscore.py
+++ project/autoscore.py
@@ -11,14 +11,12 @@ def init():
     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automlmodel.pkl')
     model = joblib.load(model_path)
 
-
 def run(raw_data):
     try:
         data = json.loads(raw_data)['data']
         data = pd.DataFrame.from_dict(data)
         # make prediction
         mypredict = model.predict(data)
-        # you can return any data type as long as it is JSON-serializable
         return mypredict.tolist()
     except Exception as ex:
         error = str(ex)
